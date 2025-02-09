0fcfb278967134b812885f7efa301af9b6f24ac6
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-09 21:58:19 +0100

    updates to Transfer.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/model/Transfer.kt
@@ -0,0 +1,15 @@
+package com.example.inventory.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.INVENTORY_TABLE
+import java.util.Date
+
+@Entity(tableName = INVENTORY_TABLE)
+data class Transfer(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val transferQuantity: Int,
+    val dispatchDate: Date,
+    val receiptDate: Date
+)
