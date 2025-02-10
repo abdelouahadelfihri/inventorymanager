129148df3f7e7000338e0438aac729a68cfeb8d2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-10 21:49:46 +0100

    updates to Order.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/Order.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/Order.kt
@@ -2,14 +2,27 @@ package com.example.inventory.domain.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.ColumnInfo
+import androidx.room.ForeignKey
+import androidx.room.Index
+import com.example.inventorymanager.domain.model.Provider
 import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
 import java.util.Date
 
-@Entity(tableName = ORDER_TABLE)
+@Entity(
+    tableName = ORDER_TABLE,
+    foreignKeys = [
+        ForeignKey(
+            entity = Provider::class,
+            parentColumns = ["idd"],
+            childColumns = ["providerId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["providerId"])]
+)
 data class Order(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val orderId: Long,
     val orderDate: Date,
-    @ColumnInfo(name = "provider_id") val idProvider: Int
+    val providerId: Long  // Foreign key referencing Provider
 )
