ba24c6bb668622a998aa02ecd052797cfb027683
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-10 22:28:59 +0100

    updates to Location.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/model/Location.kt
@@ -0,0 +1,13 @@
+package com.example.inventorymanager.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.PROVIDER_TABLE
+
+@Entity(tableName = PROVIDER_TABLE)
+data class Location(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val address: String
+)
