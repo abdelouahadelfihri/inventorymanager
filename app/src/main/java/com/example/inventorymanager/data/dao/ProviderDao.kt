97ae519cfc0bc65fc080a74dc0c8a39d03bd38f3
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-11 21:26:53 +0100

    updates to ProviderDao.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/dao/ProviderDao.kt
@@ -0,0 +1,36 @@
+package ro.alexmamo.roomjetpackcompose.data.dao
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.Companion.IGNORE
+import kotlinx.coroutines.flow.Flow
+import com.example.inventorymanager.core.Constants.Companion.BOOK_TABLE
+import com.example.inventorymanager.domain.model.Provider
+import com.example.inventorymanager.domain.repository.Books
+
+@Dao
+interface ProviderDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProvider(provider: Provider): Long
+
+    @Query("SELECT * FROM providers")
+    suspend fun getAllProviders(): List<Provider>
+
+    @Query("SELECT * FROM providers WHERE providerId = :id")
+    suspend fun getProviderById(id: Long): Provider?
+
+    @Query("SELECT * FROM $BOOK_TABLE ORDER BY id ASC")
+    fun getBooks(): Flow<Books>
+
+    @Query("SELECT * FROM $BOOK_TABLE WHERE id = :id")
+    suspend fun getBook(id: Int): Book
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addBook(book: Book)
+
+    @Update
+    suspend fun updateBook(book: Book)
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+}
