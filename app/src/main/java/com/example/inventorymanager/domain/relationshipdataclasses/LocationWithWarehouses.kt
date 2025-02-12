32421d0ef6613a1777babf833df5495951da59a0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-12 15:20:31 +0100

    updates to LocationWithWarehouses.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/LocationWithWarehouses.kt
@@ -0,0 +1,15 @@
+package com.example.inventorymanager.domain.relationshipdataclasses
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.example.inventory.domain.model.Warehouse
+import com.example.inventorymanager.domain.model.Location
+
+data class LocationWithWarehouses(
+    @Embedded val location: Location,
+    @Relation(
+        parentColumn = "locationId",
+        entityColumn = "locationOwnerId"
+    )
+    val warehouses: List<Warehouse>
+)
