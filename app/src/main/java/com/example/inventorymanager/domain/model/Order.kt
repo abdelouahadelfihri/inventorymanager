33e0b2b2064c6a9f8f38be0ab11cb48ab7243757
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-12 16:16:08 +0100

    updates to Order.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/Order.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/Order.kt
@@ -3,21 +3,22 @@ package com.example.inventory.domain.model
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
 import com.example.inventorymanager.domain.model.Provider
 import java.util.Date
 
 @Entity(
-    tableName = "orders",
+    tableName = ORDER_TABLE,
     foreignKeys = [ForeignKey(
         entity = Provider::class,
         parentColumns = ["providerId"],
         childColumns = ["providerId"],
-        onDelete = ForeignKey.SET_NULL // If a provider is deleted, the orders remain with a NULL providerId
+        onDelete = ForeignKey.CASCADE
     )]
 )
 data class Order(
     @PrimaryKey(autoGenerate = true)
-    val orderId: Int = 0,
-    val providerId: Int?, // Nullable to allow unassigned orders
+    val orderId: Int,
+    val providerId: Int,
     val orderDate: Date
 )
