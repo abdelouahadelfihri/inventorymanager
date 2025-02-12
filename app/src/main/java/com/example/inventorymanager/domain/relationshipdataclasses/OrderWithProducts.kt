a9872da2ac7ecbca283e21a78d13814174cea3f9
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-12 15:47:23 +0100

    updates to OrderWithProducts.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/OrderWithProducts.kt
@@ -0,0 +1,18 @@
+package com.example.inventorymanager.domain.relationshipdataclasses
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import androidx.room.Junction
+import com.example.inventory.domain.model.Order
+import com.example.inventorymanager.domain.model.Product
+import com.example.inventorymanager.domain.model.OrderDetails
+
+data class OrderWithProducts(
+    @Embedded val order: Order,
+    @Relation(
+        parentColumn = "orderId",
+        entityColumn = "productId",
+        associateBy = Junction(OrderDetails::class)
+    )
+    val products: List<Product>
+)
