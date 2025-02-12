cdfd2763edb718c61137b0933c7481cf7685afa0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-12 15:12:17 +0100

    updates to CustomerWithDeliveries.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/CustomerWithDeliveries.kt
@@ -0,0 +1,16 @@
+package com.example.inventorymanager.domain.relationshipdataclasses
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.example.inventory.domain.model.Delivery
+import com.example.inventorymanager.domain.model.Customer
+
+data class CustomerWithDeliveries(
+    @Embedded val customer: Customer,
+
+    @Relation(
+        parentColumn = "customerId",
+        entityColumn = "customerId"
+    )
+    val deliveries: List<Delivery>
+)
