7d9a87d962868ee03a985360e5aa03596513fec0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-12 16:08:18 +0100

    updates to Customer.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/Customer.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/Customer.kt
@@ -7,7 +7,7 @@ import com.example.inventorymanager.core.Constants.Companion.PROVIDER_TABLE
 @Entity(tableName = PROVIDER_TABLE)
 data class Customer(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val customerId: Int,
     val name: String,
     val address: String
 )
