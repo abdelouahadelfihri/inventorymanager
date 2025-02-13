c2995bc8a8a6fc944a0c36fdbac5ac9ed9e5f44b
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 15:04:38 +0100

    updates to OrderDetailsDao.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/dao/OrderDetailsDao.kt
@@ -0,0 +1,19 @@
+package ro.alexmamo.roomjetpackcompose.data.dao
+
+import androidx.room.*
+import com.example.inventory.domain.model.Order
+import com.example.inventory.domain.model.OrderDetails
+import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.domain.relationshipdataclasses.OrderWithProducts
+
+
+@Dao
+interface OrderDetailsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrderDetails(orderDetails: OrderDetails)
+
+    @Query("SELECT * FROM order_details WHERE orderId = :orderId")
+    suspend fun getOrderDetailsForOrder(orderId: Long): List<OrderDetails>
+
+}
