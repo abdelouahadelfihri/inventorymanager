602849ff90693848ed61de5829c36b330ab9dccd
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 21:07:11 +0100

    updates to LocationRepository.kt

--- app/src/main/java/com/example/inventorymanager/domain/repository/LocationRepository.kt
+++ app/src/main/java/com/example/inventorymanager/domain/repository/LocationRepository.kt
@@ -1,6 +1,5 @@
 package com.example.inventorymanager.domain.repository
 
-import com.example.inventory.domain.model.Delivery
 import com.example.inventorymanager.domain.model.Location
 import kotlinx.coroutines.flow.Flow
 
