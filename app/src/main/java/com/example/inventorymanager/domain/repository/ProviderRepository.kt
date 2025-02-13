3d3ec87795a770664947db467f03c1cb770c8733
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 20:46:18 +0100

    updates to ProviderRepository.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/repository/ProviderRepository.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.domain.repository
+
+import com.example.inventorymanager.domain.model.Provider
+import kotlinx.coroutines.flow.Flow
+
+
+typealias Providers = List<Provider>
+
+interface ProviderRepository {
+
+    fun getProvidersFromRoom(): Flow<Providers>
+
+    suspend fun getProviderFromRoom(id: Int): Provider
+
+    suspend fun addProviderToRoom(provider: Provider)
+
+    suspend fun updateProviderInRoom(provider: Provider)
+
+    suspend fun deleteProviderFromRoom(provider: Provider)
+
+}
