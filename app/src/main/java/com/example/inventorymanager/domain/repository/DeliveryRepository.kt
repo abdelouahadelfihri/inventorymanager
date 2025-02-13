e16a3c1a15a2fcd6a93c786633e56d77b3abc03e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 20:54:26 +0100

    updates to DeliveryRepository.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/repository/DeliveryRepository.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.domain.repository
+
+import com.example.inventory.domain.model.Delivery
+import kotlinx.coroutines.flow.Flow
+
+
+typealias Deliveries = List<Delivery>
+
+interface DeliveryRepository {
+
+    fun getDeliveriesFromRoom(): Flow<Deliveries>
+
+    suspend fun getDeliveryFromRoom(id: Int): Delivery
+
+    suspend fun addDeliveryToRoom(delivery: Delivery)
+
+    suspend fun updateDeliveryInRoom(delivery: Delivery)
+
+    suspend fun deleteDeliveryFromRoom(delivery: Delivery)
+
+}
