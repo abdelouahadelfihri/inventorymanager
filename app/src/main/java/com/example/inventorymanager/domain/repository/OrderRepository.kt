ba66d1e7e68c1eb44f0687472156155f7bab153f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 20:49:58 +0100

    updates to OrderRepository.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/repository/OrderRepository.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.domain.repository
+
+import com.example.inventory.domain.model.Order
+import kotlinx.coroutines.flow.Flow
+
+
+typealias Orders = List<Order>
+
+interface OrderRepository {
+
+    fun getOrdersFromRoom(): Flow<Orders>
+
+    suspend fun getOrderFromRoom(id: Int): Order
+
+    suspend fun addOrderToRoom(order: Order)
+
+    suspend fun updateOrderInRoom(order: Order)
+
+    suspend fun deleteOrderFromRoom(order: Order)
+
+}
