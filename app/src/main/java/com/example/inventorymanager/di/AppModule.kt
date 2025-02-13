4458d6bb17ec087592a39cf65ba692c261a39aad
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 15:28:43 +0100

    updates to AppModule.kt

--- app/src/main/java/com/example/inventorymanager/di/AppModule.kt
+++ app/src/main/java/com/example/inventorymanager/di/AppModule.kt
@@ -7,34 +7,41 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import ro.alexmamo.roomjetpackcompose.core.Constants.Companion.BOOK_TABLE
-import ro.alexmamo.roomjetpackcompose.data.dao.BookDao
-import ro.alexmamo.roomjetpackcompose.data.network.BookDb
-import ro.alexmamo.roomjetpackcompose.data.repository.BookRepositoryImpl
-import ro.alexmamo.roomjetpackcompose.domain.repository.BookRepository
+import com.example.inventorymanager.core.Constants.Companion.BOOK_TABLE
+import com.example.inventorymanager.data.dao.BookDao
+import com.example.inventorymanager.data.network.AppDatabase
+import com.example.inventorymanager.data.repository.BookRepositoryImpl
+import com.example.inventorymanager.domain.repository.BookRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
-    @Provides
-    fun provideBookDb(
-        @ApplicationContext
-        context: Context
-    ) = Room.databaseBuilder(
-        context,
-        BookDb::class.java,
-        BOOK_TABLE
-    ).build()
 
     @Provides
-    fun provideBookDao(
-        bookDb: BookDb
-    ) = bookDb.bookDao
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database"
+        ).build()
+    }
 
     @Provides
-    fun provideBookRepository(
-        bookDao: BookDao
-    ): BookRepository = BookRepositoryImpl(
-        bookDao = bookDao
-    )
+    fun provideBookDao(database: AppDatabase): BookDao = database.bookDao()
+
+    @Provides
+    fun provideAuthorDao(database: AppDatabase): AuthorDao = database.authorDao()
+
+    @Provides
+    fun providePublisherDao(database: AppDatabase): PublisherDao = database.publisherDao()
+
+    @Provides
+    fun provideBookRepository(bookDao: BookDao): BookRepository = BookRepositoryImpl(bookDao)
+
+    @Provides
+    fun provideAuthorRepository(authorDao: AuthorDao): AuthorRepository = AuthorRepositoryImpl(authorDao)
+
+    @Provides
+    fun providePublisherRepository(publisherDao: PublisherDao): PublisherRepository =
+        PublisherRepositoryImpl(publisherDao)
 }
