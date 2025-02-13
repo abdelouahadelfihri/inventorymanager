ad00ac01006db8ae57a8b933b619bda7419baf19
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-13 20:32:23 +0100

    updates to CustomerRepository.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/repository/CustomerRepository.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.domain.repository
+
+import com.example.inventorymanager.domain.model.Customer
+import kotlinx.coroutines.flow.Flow
+
+
+typealias Customers = List<Customer>
+
+interface CustomerRepository {
+
+    fun getCustomersFromRoom(): Flow<Customers>
+
+    suspend fun getCustomerFromRoom(id: Int): Customer
+
+    suspend fun addCustomerToRoom(customer: Customer)
+
+    suspend fun updateCustomerInRoom(customer: Customer)
+
+    suspend fun deleteCustomerFromRoom(customer: Customer)
+
+}
