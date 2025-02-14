e040484851919c7678ec4e8d33ac47352a7e7d57
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-14 15:35:44 +0100

    updates to Screen.kt

--- app/src/main/java/com/example/inventorymanager/navigation/Screen.kt
+++ app/src/main/java/com/example/inventorymanager/navigation/Screen.kt
@@ -1,7 +1,7 @@
 package ro.alexmamo.roomjetpackcompose.navigation
 
-import ro.alexmamo.roomjetpackcompose.core.Constants.Companion.BOOKS_SCREEN
-import ro.alexmamo.roomjetpackcompose.core.Constants.Companion.UPDATE_BOOK_SCREEN
+import com.example.inventorymanager.core.Constants.Companion.BOOKS_SCREEN
+import com.example.inventorymanager.core.Constants.Companion.UPDATE_BOOK_SCREEN
 
 sealed class Screen(val route: String) {
     object BooksScreen: Screen(BOOKS_SCREEN)
