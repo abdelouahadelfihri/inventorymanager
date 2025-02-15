a4e0050ee87a0e79074133324bb6a98bbbb3d8f4
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-15 14:12:47 +0100

    updates to DeliveryDetailsDao.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/dao/DeliveryDetailsDao.kt
@@ -0,0 +1,32 @@
+package com.example.inventorymanager.data.dao
+
+import androidx.room.*
+import com.example.inventory.domain.model.DeliveryDetails
+import com.example.inventorymanager.core.Constants.Companion.DELIVERY_DETAILS_TABLE
+
+
+
+@Dao
+interface DeliveryDetailsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(deliveryDetails: DeliveryDetails)
+
+    @Update
+    suspend fun update(deliveryDetails: DeliveryDetails)
+
+    @Delete
+    suspend fun delete(deliveryDetails: DeliveryDetails)
+
+    @Query("SELECT * FROM delivery_details WHERE deliveryId = :deliveryId")
+    suspend fun getByDeliveryId(deliveryId: Int): List<DeliveryDetails>
+
+    @Query("SELECT * FROM delivery_details WHERE productId = :productId")
+    suspend fun getByProductId(productId: Int): List<DeliveryDetails>
+
+    @Query("SELECT * FROM delivery_details WHERE warehouseId = :warehouseId")
+    suspend fun getByWarehouseId(warehouseId: Int): List<DeliveryDetails>
+
+    @Query("SELECT * FROM delivery_details WHERE deliveryId = :deliveryId AND productId = :productId AND warehouseId = :warehouseId")
+    suspend fun getByIds(deliveryId: Int, productId: Int, warehouseId: Int): DeliveryDetails?
+}
