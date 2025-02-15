45eb3f99eda668136d1e11492a2e3f27cccc47b2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-15 14:09:21 +0100

    updates to OrderDetails.kt

--- app/src/main/java/com/example/inventorymanager/data/dao/OrderDetailsDao.kt
+++ app/src/main/java/com/example/inventorymanager/data/dao/OrderDetailsDao.kt
@@ -6,20 +6,29 @@ import com.example.inventorymanager.domain.model.Order
 import com.example.inventorymanager.domain.model.OrderDetails
 
 
+import androidx.room.*
+
 @Dao
 interface OrderDetailsDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(orderDetails: OrderDetails)
 
     @Update
-    suspend fun update(order: Order)
+    suspend fun update(orderDetails: OrderDetails)
 
     @Delete
-    suspend fun delete(order: Order)
+    suspend fun delete(orderDetails: OrderDetails)
 
-    @Query("SELECT * FROM $ORDER_DETAILS_TABLE")
-    suspend fun getAll(): List<Order>
+    @Query("SELECT * FROM order_details WHERE orderId = :orderId")
+    suspend fun getByOrderId(orderId: Int): List<OrderDetails>
 
-    @Query("SELECT * FROM $ORDER_DETAILS_TABLE WHERE orderId = :id")
-    suspend fun getByIdS(id: Int): Order?
+    @Query("SELECT * FROM order_details WHERE productId = :productId")
+    suspend fun getByProductId(productId: Int): List<OrderDetails>
+
+    @Query("SELECT * FROM order_details WHERE warehouseId = :warehouseId")
+    suspend fun getByWarehouseId(warehouseId: Int): List<OrderDetails>
+
+    @Query("SELECT * FROM order_details WHERE orderId = :orderId AND productId = :productId AND warehouseId = :warehouseId")
+    suspend fun getByIds(orderId: Int, productId: Int, warehouseId: Int): OrderDetails?
 }
