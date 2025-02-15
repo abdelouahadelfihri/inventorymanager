4b64d2561a65228eb3795b8dfeb9a34d0a230a59
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-15 13:18:30 +0100

    updates to WarehouseRepositoryImpl.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/repository/WarehouseRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package ro.alexmamo.roomjetpackcompose.data.repository
+
+import ro.alexmamo.roomjetpackcompose.data.dao.BookDao
+import ro.alexmamo.roomjetpackcompose.domain.model.Book
+import ro.alexmamo.roomjetpackcompose.domain.repository.BookRepository
+
+class WarehouseRepositoryImpl(
+    private val bookDao: BookDao
+) : BookRepository {
+    override fun getBooksFromRoom() = bookDao.getBooks()
+
+    override suspend fun getBookFromRoom(id: Int) = bookDao.getBook(id)
+
+    override suspend fun addBookToRoom(book: Book) = bookDao.addBook(book)
+
+    override suspend fun updateBookInRoom(book: Book) = bookDao.updateBook(book)
+
+    override suspend fun deleteBookFromRoom(book: Book) = bookDao.deleteBook(book)
+}
