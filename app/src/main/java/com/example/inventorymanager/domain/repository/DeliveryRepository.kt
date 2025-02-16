b22b19f6f0736778df988c3abddb5c7d2933d16f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-16 15:26:35 +0100

    updates to DeliveryRepository.kt

--- app/src/main/java/com/example/inventorymanager/domain/repository/DeliveryRepository.kt
+++ app/src/main/java/com/example/inventorymanager/domain/repository/DeliveryRepository.kt
@@ -1,6 +1,6 @@
 package com.example.inventorymanager.domain.repository
 
-import com.example.inventory.domain.model.Delivery
+import com.example.inventorymanager.domain.model.Delivery
 import kotlinx.coroutines.flow.Flow
 
 
