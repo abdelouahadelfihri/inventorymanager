b27dfa127f05ec0a0a339589b1dfd28971a28a5f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-16 15:42:20 +0100

    updates to Transfer.kt

--- app/src/main/java/com/example/inventorymanager/data/dao/TransferDao.kt
+++ app/src/main/java/com/example/inventorymanager/data/dao/TransferDao.kt
@@ -6,27 +6,27 @@ import androidx.room.Query
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.IGNORE
 import androidx.room.Update
-import com.example.inventorymanager.domain.model.Order
+import com.example.inventorymanager.domain.model.Transfer
 import com.example.inventorymanager.core.Constants.Companion.TRANSFER_TABLE
-import com.example.inventorymanager.domain.repository.Orders
+import com.example.inventorymanager.domain.repository.Transfers
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TransferDao {
 
     @Query("SELECT * FROM $TRANSFER_TABLE ORDER BY transferId ASC")
-    fun getOrders(): Flow<Orders>
+    fun getTransfers(): Flow<Transfers>
 
-    @Query("SELECT * FROM $TRANSFER_TABLE WHERE orderId = :id")
-    suspend fun getOrder(id: Int): Order
+    @Query("SELECT * FROM $TRANSFER_TABLE WHERE transferId = :id")
+    suspend fun getTransfer(id: Int): Transfer
 
     @Insert(onConflict = IGNORE)
-    suspend fun addOrder(order: Order)
+    suspend fun addTransfer(order: Transfer)
 
     @Update
-    suspend fun updateOrder(order: Order)
+    suspend fun updateTransfer(order: Transfer)
 
     @Delete
-    suspend fun deleteOrder(order: Order)
+    suspend fun deleteTransfer(order: Transfer)
 
 }
