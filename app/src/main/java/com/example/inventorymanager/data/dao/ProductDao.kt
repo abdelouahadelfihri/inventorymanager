2b09782a2f1b31590bc7a09c1b55708e6424cda1
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-02-19 16:02:11 +0100

    updates to ProductDao.kt

--- app/src/main/java/com/example/inventorymanager/data/dao/ProductDao.kt
+++ app/src/main/java/com/example/inventorymanager/data/dao/ProductDao.kt
@@ -6,8 +6,10 @@ import androidx.room.Query
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.IGNORE
 import androidx.room.Update
+import com.example.inventorymanager.core.Constants.Companion.LOCATION_TABLE
 import com.example.inventorymanager.domain.model.Product
 import com.example.inventorymanager.core.Constants.Companion.PRODUCT_TABLE
+import com.example.inventorymanager.domain.model.Location
 import com.example.inventorymanager.domain.repository.Products
 import kotlinx.coroutines.flow.Flow
 
@@ -28,5 +30,8 @@ interface ProductDao {
 
     @Delete
     suspend fun deleteProduct(id: Int)
+
+    @Query("SELECT * FROM $PRODUCT_TABLE WHERE name LIKE :searchQuery")
+    fun searchProducts(searchQuery: String): Flow<List<Product>>
 
 }
