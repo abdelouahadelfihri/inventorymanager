51fb7989d75569592660b0010cab1b8c07429962
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-04-05 17:32:34 +0000

    updates to CustomerCard.kt

--- app/src/main/java/com/example/inventorymanager/presentation/customers/list/components/CustomerCard.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/customers/list/components/CustomerCard.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -46,7 +45,7 @@ fun CustomerCard(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                text = "Title",
+                text = "${customer.customerId}",
                 modifier = Modifier.weight(1f),
                 color = Color.White,
                 fontWeight = FontWeight.Bold
