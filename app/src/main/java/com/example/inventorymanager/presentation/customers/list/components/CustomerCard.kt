2dfb270a09fdd566e0b88d1eed1198425e616ae6
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-04-05 17:35:22 +0000

    updates to CustomerCard.kt

--- app/src/main/java/com/example/inventorymanager/presentation/customers/list/components/CustomerCard.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/customers/list/components/CustomerCard.kt
@@ -1,4 +1,4 @@
-package com.example.inventorymanager.presentation.books.components
+package com.example.inventorymanager.presentation.customers.list.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
