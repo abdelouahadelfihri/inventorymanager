e80350176c092fd6ad33ee9754c8201653f946be
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-04-09 20:52:53 +0100

    updates to AddDeliveryTopBar.kt

--- app/src/main/java/com/example/inventorymanager/presentation/deliveries/add/components/AddDeliveryTopBar.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/deliveries/add/components/AddDeliveryTopBar.kt
@@ -1,4 +1,4 @@
-package ro.alexmamo.roomjetpackcompose.presentation.update_book.components
+package com.example.inventorymanager.presentation.customers.add.components
 
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -7,10 +7,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
-import ro.alexmamo.roomjetpackcompose.core.Constants.Companion.UPDATE_BOOK_SCREEN
+import com.example.inventorymanager.core.Constants.Companion.UPDATE_BOOK_SCREEN
 
 @Composable
-fun UpdateBookTopBar(
+fun AddDeliveryTopBar(
     navigateBack: () -> Unit
 ) {
     TopAppBar (
