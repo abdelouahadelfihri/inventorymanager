614521526708180b69cd5ad52fde6d58394e0320
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-11 19:37:14 +0100

    updates to UpdateCustomerContent.kt

--- app/src/main/java/com/example/inventorymanager/presentation/customers/details/components/UpdateCustomerContent.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/customers/details/components/UpdateCustomerContent.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +12,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.inventorymanager.domain.model.Customer
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 
 @Composable
 fun UpdateCustomerContent(
