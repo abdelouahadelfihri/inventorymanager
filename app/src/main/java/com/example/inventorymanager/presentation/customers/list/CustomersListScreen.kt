74ae0334ee9d6ccd121026f68b3b75517740da90
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-14 20:03:12 +0100

    updates to CustomersListScreen.kt

--- app/src/main/java/com/example/inventorymanager/presentation/customers/list/CustomersListScreen.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/customers/list/CustomersListScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
