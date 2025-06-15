a7582adec88d593a3d058a4a10f28560ec051708
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-15 11:14:30 +0100

    updates to AddDeliveryScreen.kt

--- app/src/main/java/com/example/inventorymanager/presentation/deliveries/add/AddDeliveryScreen.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/deliveries/add/AddDeliveryScreen.kt
@@ -4,13 +4,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.inventorymanager.presentation.customers.CustomersViewModel
+import com.example.inventorymanager.presentation.deliveries.DeliveriesViewModel
 import com.example.inventorymanager.presentation.deliveries.add.components.AddDeliveryContent
 import com.example.inventorymanager.presentation.deliveries.add.components.AddDeliveryTopBar
 
 @Composable
 fun AddDeliveryScreen(
-    viewModel: CustomersViewModel = hiltViewModel(),
+    viewModel: DeliveriesViewModel = hiltViewModel(),
     navigateBack: () -> Unit
 ) {
     Scaffold(
