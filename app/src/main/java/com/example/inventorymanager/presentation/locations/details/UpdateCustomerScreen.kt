383fe8a8e4f39cffa4f74b64cb462d33fc28ec04
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-17 20:04:00 +0100

    updates to locations

---
+++ app/src/main/java/com/example/inventorymanager/presentation/locations/details/UpdateCustomerScreen.kt
@@ -0,0 +1,40 @@
+package com.example.inventorymanager.presentation.locations.details
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.inventorymanager.presentation.customers.CustomersViewModel
+import com.example.inventorymanager.presentation.customers.details.components.UpdateCustomerContent
+import com.example.inventorymanager.presentation.customers.details.components.UpdateCustomerTopBar
+
+@Composable
+fun UpdateCustomerScreen(
+    viewModel: CustomersViewModel = hiltViewModel(),
+    customerId: Int,
+    navigateBack: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        viewModel.getCustomer(customerId)
+    }
+    Scaffold(
+        topBar = {
+            UpdateCustomerTopBar(
+                navigateBack = navigateBack
+            )
+        },
+        content = { padding ->
+            UpdateCustomerContent(
+                padding = padding,
+                customer = viewModel.customer,
+                updateCustomer = { customer ->
+                    viewModel.updateCustomer(customer)
+                },
+                deleteCustomer = { customerId ->
+                    viewModel.deleteCustomer(customerId)
+                },
+                navigateBack = navigateBack
+            )
+        }
+    )
+}
