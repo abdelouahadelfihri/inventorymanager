9afbe46a6f344204cdb68f1933421d5e1985f534
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-18 19:16:36 +0100

    updates to AddLocationScreen.kt

---
+++ app/src/main/java/com/example/inventorymanager/presentation/locations/add/AddLocationScreen.kt
@@ -0,0 +1,29 @@
+package com.example.inventorymanager.presentation.locations.add
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun AddLocationScreen(
+    viewModel: LocationsViewModel = hiltViewModel(),
+    navigateBack: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            AddLocationTopBar(
+                navigateBack = navigateBack
+            )
+        },
+        content = { padding ->
+            AddLocationContent(
+                padding = padding,
+                location = viewModel.location,
+                addLocation = { location ->
+                    viewModel.addLocation(location)
+                },
+                navigateBack = navigateBack
+            )
+        }
+    )
+}
