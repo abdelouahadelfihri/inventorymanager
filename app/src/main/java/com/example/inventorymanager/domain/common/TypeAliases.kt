e296bc91d0fed3e4637c3cfba6baabc9f982b8a2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-19 20:16:50 +0100

    updates to TypeAliases.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/common/TypeAliases.kt
@@ -0,0 +1,7 @@
+package com.example.inventorymanager.domain.common
+
+import com.example.inventorymanager.domain.model.Customer
+import com.example.inventorymanager.domain.model.Delivery
+
+typealias Customers = List<Customer>
+typealias Deliveries = List<Delivery>
