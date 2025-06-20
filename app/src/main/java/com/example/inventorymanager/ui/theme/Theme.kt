5365d9e1dd694ab1bd1fec173969a4de99b401cb
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-20 20:32:46 +0100

    updates to Theme.kt

--- app/src/main/java/com/example/inventorymanager/ui/theme/Theme.kt
+++ app/src/main/java/com/example/inventorymanager/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 
 // --- Colors ---
 private val LightColorPalette = lightColors(
