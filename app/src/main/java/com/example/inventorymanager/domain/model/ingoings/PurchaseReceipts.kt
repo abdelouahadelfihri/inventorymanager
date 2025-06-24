5b4483dface8996f9a4f38e3a60787b819efcffd
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-24 19:47:07 +0100

    updates to PurchaseReceipts.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseReceipts.kt
@@ -0,0 +1,35 @@
+package com.example.inventorymanager.domain.model.ingoings
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.domain.model.Warehouse
+import java.util.Date
+
+@Entity(
+    tableName = ORDER_TABLE,
+    foreignKeys = [
+        ForeignKey(
+        entity = Order::class,
+        parentColumns = ["orderId"],
+        childColumns = ["orderId"],
+        onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Warehouse::class,
+            parentColumns = ["warehouseId"],
+            childColumns = ["warehouseId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class PurchaseReceipts(
+    @PrimaryKey(autoGenerate = true)
+    val purchaseReceiptId: Int,
+    var orderId: Int,
+    val receiptDate: Date,
+    val warehouseId: Int,
+    val receivedBy: String,
+    val notes: String
+)
