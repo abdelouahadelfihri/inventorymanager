7ee1d64d7e1b2a82d3a80bdcfdd95d2ab0f7b231
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-25 19:28:28 +0100

    updates to Item.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/Item.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/Item.kt
@@ -3,6 +3,7 @@ package com.example.inventorymanager.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.inventorymanager.core.Constants.Companion.PRODUCT_TABLE
+import java.time.LocalDateTime
 
 @Entity(tableName = PRODUCT_TABLE)
 data class Item(
@@ -11,13 +12,17 @@ data class Item(
     val code: String,
     val barCode: String,
     val name: String,
+    val sku: String,
+    val category: Int,
+    val unit: Int,
     val description: String,
-    val category: String,
     val reorderQuantity: Int,
     val reorderLevel: Int,
     val packedWeight: Double,
     val packedHeight: Double,
     val packedWidth: Double,
     val packedDepth: Double,
-    val refrigerated: Boolean
+    val refrigerated: Boolean,
+    val isActive: Boolean,
+    val createdAt: LocalDateTime, // ✅ this is the DateTime field
 )
