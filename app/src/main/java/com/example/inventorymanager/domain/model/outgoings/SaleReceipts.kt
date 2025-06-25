7bd9ec3c3a8f0ec49086304493d5c1666ead241b
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-25 18:51:08 +0100

    updates to SaleReceipts.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/model/outgoings/SaleReceipts.kt
@@ -0,0 +1,45 @@
+package com.example.inventorymanager.domain.model.outgoings
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.domain.model.Warehouse
+import com.example.inventorymanager.domain.model.ingoings.Order
+import java.util.Date
+
+@Entity(
+    tableName = ORDER_TABLE,
+    foreignKeys = [
+        ForeignKey(
+        entity = Order::class,
+        parentColumns = ["orderId"],
+        childColumns = ["orderId"],
+        onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Warehouse::class,
+            parentColumns = ["warehouseId"],
+            childColumns = ["warehouseId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Customer::class,
+            parentColumns = ["customerId"],
+            childColumns = ["customerId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class SaleReceipts(
+    @PrimaryKey(autoGenerate = true)
+    val saleReceiptId: Int,
+    var orderId: Int,
+    val customerId: Int,
+    val wareHouseId: Int,
+    val receiptDate: Date,
+    val deliveredBy: String,
+    val totalAmount: Double,
+    val notes: String,
+    val status: String
+)
