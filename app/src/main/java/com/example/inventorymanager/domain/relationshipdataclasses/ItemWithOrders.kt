ea42ec86adc1f49897c3072804b4793b638c1a11
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-25 19:11:35 +0100

    updates to ItemWithOrders.kt

--- app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/ItemWithOrders.kt
+++ app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/ItemWithOrders.kt
@@ -3,14 +3,14 @@ package com.example.inventorymanager.domain.relationshipdataclasses
 import androidx.room.Embedded
 import androidx.room.Relation
 import androidx.room.Junction
-import com.example.inventory.domain.model.Order
-import com.example.inventory.domain.model.OrderDetails
+import com.example.inventorymanager.domain.model.ingoings.OrderDetails
+import com.example.inventorymanager.domain.model.ingoings.Order
 import com.example.inventorymanager.domain.model.Item
 
-data class ProductWithOrders(
+data class ItemWithOrders(
     @Embedded val item: Item,
     @Relation(
-        parentColumn = "productId",
+        parentColumn = "itemId",
         entityColumn = "orderId",
         associateBy = Junction(OrderDetails::class)
     )
