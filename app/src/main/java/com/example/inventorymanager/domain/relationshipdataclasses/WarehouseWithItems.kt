664328f205e8027c605495f2bf95816d0ba7581d
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-25 19:15:22 +0100

    updates to WarehouseWithItems.kt

--- app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/WarehouseWithItems.kt
+++ app/src/main/java/com/example/inventorymanager/domain/relationshipdataclasses/WarehouseWithItems.kt
@@ -3,20 +3,20 @@ package com.example.inventorymanager.domain.relationshipdataclasses
 import androidx.room.Embedded
 import androidx.room.Relation
 import androidx.room.Junction
-import com.example.inventory.domain.model.Inventory
-import com.example.inventory.domain.model.Warehouse
+import com.example.inventorymanager.domain.model.Inventory
+import com.example.inventorymanager.domain.model.Warehouse
 import com.example.inventorymanager.domain.model.Item
 
 
-data class WarehouseWithProducts(
+data class WarehouseWithItems(
     @Embedded val warehouse: Warehouse,
     @Relation(
         parentColumn = "warehouseId",
-        entityColumn = "productId",
+        entityColumn = "itemId",
         associateBy = Junction(
             Inventory::class,
-            parentColumn = "warehouse_id",
-            entityColumn = "product_id"
+            parentColumn = "warehouseId",
+            entityColumn = "itemId"
         )
     )
     val items: List<Item>
