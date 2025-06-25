a1d7a92d74615889e7234a0f9c44e499a17d9093
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-25 19:03:02 +0100

    updates to ReturnsFromCustomer.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/ReturnsFromCustomer.kt
@@ -0,0 +1,42 @@
+package com.example.inventorymanager.domain.model.ingoings
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.domain.model.Warehouse
+import com.example.inventorymanager.domain.model.outgoings.Customer
+import java.util.Date
+
+@Entity(
+    tableName = ORDER_TABLE,
+    foreignKeys = [
+        ForeignKey(
+            entity = Item::class,
+            parentColumns = ["itemId"],
+            childColumns = ["itemId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Warehouse::class,
+            parentColumns = ["warehouseId"],
+            childColumns = ["warehouseId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Customer::class,
+            parentColumns = ["customerId"],
+            childColumns = ["customerId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ReturnsFromCustomer(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var customerId: Int,
+    val itemId: Int,
+    val quantity: Int,
+    val returnDate: Date,
+    val reason: String
+)
