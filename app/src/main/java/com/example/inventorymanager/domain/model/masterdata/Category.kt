44deee053bef5ec020a71c78be8ea7424572de42
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-28 18:52:05 +0100

    updates to Category.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/masterdata/Category.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/masterdata/Category.kt
@@ -2,9 +2,9 @@ package com.example.inventorymanager.domain.model.masterdata
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.inventorymanager.core.Constants.Companion.PRODUCT_TABLE
+import com.example.inventorymanager.core.Constants.Companion.CATEGORY_TABLE
 
-@Entity(tableName = PRODUCT_TABLE)
+@Entity(tableName = CATEGORY_TABLE)
 data class Category (
     @PrimaryKey(autoGenerate = true)
     val Id: Int,
