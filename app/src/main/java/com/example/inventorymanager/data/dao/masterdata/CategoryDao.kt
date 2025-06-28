69f7817c0c07b9318a92be1ce696ccbd56c5808e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-28 18:54:59 +0100

    updates to CategoryDao.kt

--- app/src/main/java/com/example/inventorymanager/data/dao/masterdata/CategoryDao.kt
+++ app/src/main/java/com/example/inventorymanager/data/dao/masterdata/CategoryDao.kt
@@ -1,26 +1,26 @@
-package com.example.inventorymanager.data.dao
+package com.example.inventorymanager.data.dao.masterdata
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Query
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.Companion.IGNORE
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
-import com.example.inventorymanager.domain.model.masterdata.Category
-import com.example.inventorymanager.core.Constants.Companion.CATEGORY_TABLE
+import com.example.inventorymanager.core.Constants
 import com.example.inventorymanager.domain.common.Categories
+import com.example.inventorymanager.domain.model.masterdata.Category
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CategoryDao {
 
-    @Query("SELECT * FROM $CATEGORY_TABLE ORDER BY id ASC")
+    @Query("SELECT * FROM ${Constants.Companion.CATEGORY_TABLE} ORDER BY id ASC")
     fun getCategories(): Flow<Categories>
 
-    @Query("SELECT * FROM $CATEGORY_TABLE WHERE id = :id")
+    @Query("SELECT * FROM ${Constants.Companion.CATEGORY_TABLE} WHERE id = :id")
     suspend fun getCategory(id: Int): Category
 
-    @Insert(onConflict = IGNORE)
+    @Insert(onConflict = OnConflictStrategy.Companion.IGNORE)
     suspend fun addCategory(category: Category)
 
     @Update
