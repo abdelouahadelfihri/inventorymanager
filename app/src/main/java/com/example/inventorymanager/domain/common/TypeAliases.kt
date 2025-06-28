cef3d762ff68657b6e0c1ab49e092f37e23592d7
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-28 18:49:12 +0100

    updates to TypeAliases.kt

--- app/src/main/java/com/example/inventorymanager/domain/common/TypeAliases.kt
+++ app/src/main/java/com/example/inventorymanager/domain/common/TypeAliases.kt
@@ -3,7 +3,10 @@ package com.example.inventorymanager.domain.common
 import com.example.inventorymanager.domain.model.outgoings.Customer
 import com.example.inventorymanager.domain.model.Delivery
 import com.example.inventorymanager.domain.model.ingoings.Order
+import com.example.inventorymanager.domain.model.masterdata.Category
+import com.example.inventorymanager.domain.model.outgoings.Delivery
 
 typealias Customers = List<Customer>
 typealias Deliveries = List<Delivery>
 typealias Orders = List<Order>
+typealias Categories = List<Category>
