bc28a2cf603ea7a05ce30299576284fa45a7c188
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-29 18:48:26 +0100

    updates to ReturnsFromCustomer.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/ingoings/ReturnsFromCustomer.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/ReturnsFromCustomer.kt
@@ -3,14 +3,14 @@ package com.example.inventorymanager.domain.model.ingoings
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.core.Constants.Companion.RETURNS_FROM_CUSTOMER_TABLE
 import com.example.inventorymanager.domain.model.masterdata.Warehouse
 import com.example.inventorymanager.domain.model.masterdata.Item
 import com.example.inventorymanager.domain.model.outgoings.Customer
 import java.util.Date
 
 @Entity(
-    tableName = ORDER_TABLE,
+    tableName = RETURNS_FROM_CUSTOMER_TABLE,
     foreignKeys = [
         ForeignKey(
             entity = Item::class,
