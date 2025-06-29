a19b04275fc6fab54e6b14f399a96bc3dd7f146f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-29 18:35:48 +0100

    updates to PurchaseReceipts.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseReceipts.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseReceipts.kt
@@ -3,12 +3,12 @@ package com.example.inventorymanager.domain.model.ingoings
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.example.inventorymanager.core.Constants.Companion.ORDER_TABLE
+import com.example.inventorymanager.core.Constants.Companion.PURCHASE_RECEIPT_TABLE
 import com.example.inventorymanager.domain.model.masterdata.Warehouse
 import java.util.Date
 
 @Entity(
-    tableName = ORDER_TABLE,
+    tableName = PURCHASE_RECEIPT_TABLE,
     foreignKeys = [
         ForeignKey(
         entity = Order::class,
