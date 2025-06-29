4f51cadc43df9a74248b236327c4fa84d1a10ba8
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-29 19:44:31 +0100

    updates to PurchaseReceiptDetailDao.kt

--- app/src/main/java/com/example/inventorymanager/data/dao/ingoings/PurchaseReceiptDetailDao.kt
+++ app/src/main/java/com/example/inventorymanager/data/dao/ingoings/PurchaseReceiptDetailDao.kt
@@ -1,30 +1,25 @@
 package com.example.inventorymanager.data.dao.ingoings
 
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.example.inventorymanager.core.Constants.Companion.PURCHASE_RECEIPT_DETAIL_TABLE
-import com.example.inventorymanager.domain.model.ingoings.Order
-import com.example.inventorymanager.domain.repository.Orders
+import com.example.inventorymanager.domain.model.ingoings.PurchaseReceiptDetail
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface PurchaseReceiptDetailDao {
 
-    @Query("SELECT * FROM ${PURCHASE_RECEIPT_TABLE} ORDER BY orderId ASC")
-    fun getPurchaseReceipts(): Flow<Orders>
+    @Query("SELECT * FROM $PURCHASE_RECEIPT_DETAIL_TABLE WHERE purchaseReceiptId = :receiptId")
+    fun getDetailsForReceipt(receiptId: Int): Flow<List<PurchaseReceiptDetail>>
 
-    @Query("SELECT * FROM ${PURCHASE_RECEIPT_TABLE} WHERE orderId = :id")
-    suspend fun getPurchaseReceipt(id: Int): Order
-
-    @Insert(onConflict = OnConflictStrategy.Companion.IGNORE)
-    suspend fun addPurchaseReceipt(order: Order)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertDetail(detail: PurchaseReceiptDetail)
 
     @Update
-    suspend fun updatePurchaseReceipt(order: Order)
+    suspend fun updateDetail(detail: PurchaseReceiptDetail)
 
     @Delete
-    suspend fun deletePurchaseReceipt(id: Int)
+    suspend fun deleteDetail(detail: PurchaseReceiptDetail)
 
+    @Query("DELETE FROM $PURCHASE_RECEIPT_DETAIL_TABLE WHERE purchaseReceiptId = :receiptId")
+    suspend fun deleteAllDetailsForReceipt(receiptId: Int)
 }
