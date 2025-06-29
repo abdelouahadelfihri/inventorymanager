aa8d09773e2a2f4efeadf1899ead77abc089ce1e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-29 19:33:50 +0100

    updates to SaleReceiptDetailDao.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/dao/outgoings/SaleReceiptDetailDao.kt
@@ -0,0 +1,30 @@
+package com.example.inventorymanager.data.dao.outgoings
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.inventorymanager.core.Constants
+import com.example.inventorymanager.domain.common.Customers
+import com.example.inventorymanager.domain.model.outgoings.Customer
+import kotlinx.coroutines.flow.Flow
+
+interface SaleReceiptDetailDao {
+
+    @Query("SELECT * FROM ${Constants.Companion.CUSTOMER_TABLE} ORDER BY customerId ASC")
+    fun getCustomers(): Flow<Customers>
+
+    @Query("SELECT * FROM ${Constants.Companion.CUSTOMER_TABLE} WHERE customerId = :id")
+    suspend fun getCustomer(id: Int): Customer
+
+    @Insert(onConflict = OnConflictStrategy.Companion.IGNORE)
+    suspend fun addCustomer(order: Customer)
+
+    @Update
+    suspend fun updateCustomer(order: Customer)
+
+    @Delete
+    suspend fun deleteCustomer(id: Int)
+
+}
