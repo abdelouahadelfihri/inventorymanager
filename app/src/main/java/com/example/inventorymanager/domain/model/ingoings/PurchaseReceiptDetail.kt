4f1d0c853c9821f63b524781c2a78e7e22ff0dde
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-29 19:50:16 +0100

    updates to PurchaseReceiptDetail.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseReceiptDetail.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseReceiptDetail.kt
@@ -1,4 +1,5 @@
-package com.example.inventorymanager.data.local.entity.ingoings
+package com.example.inventorymanager.domain.model.ingoings
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.ForeignKey
