83a4a6632f6298b9b3d77f7e4227ecd6708826a6
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-06-30 20:25:58 +0100

    updates to ProductUnitRepository.kt

---
+++ app/src/main/java/com/example/inventorymanager/domain/repository/ProductUnitRepository.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.domain.repository.ingoings
+
+import com.example.inventorymanager.domain.model.ingoings.Provider
+import kotlinx.coroutines.flow.Flow
+
+
+typealias Units = List<Unit>
+
+interface ProductUnitRepository {
+
+    fun getUnitsFromRoom(): Flow<Units>
+
+    suspend fun getUnitFromRoom(id: Int): Unit
+
+    suspend fun addUnitToRoom(unit: Unit)
+
+    suspend fun updateProviderInRoom(provider: Provider)
+
+    suspend fun deleteProviderFromRoom(id: Int)
+
+}
