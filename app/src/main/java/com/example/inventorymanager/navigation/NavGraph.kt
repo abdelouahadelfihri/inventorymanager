a2261f82191aa6641620eae52998a35d5133e380
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-02 20:10:05 +0100

    updates to NavGraph.kt

--- app/src/main/java/com/example/inventorymanager/navigation/NavGraph.kt
+++ app/src/main/java/com/example/inventorymanager/navigation/NavGraph.kt
@@ -2,6 +2,7 @@ package com.example.inventorymanager.navigation
 
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,7 +11,10 @@ import com.example.inventorymanager.presentation.orders.list.OrdersListScreen
 import com.example.inventorymanager.presentation.outs.customers.details.UpdateCustomerScreen
 import com.example.inventorymanager.presentation.outs.customers.add.AddCustomerScreen
 import com.example.inventorymanager.presentation.dashboard.DashboardScreen
+import com.example.inventorymanager.presentation.ingoings.orders.OrdersViewModel
+import com.example.inventorymanager.presentation.ingoings.orders.add.AddOrderScreen
 import com.example.inventorymanager.presentation.locations.list.LocationsListScreen
+import com.example.inventorymanager.presentation.providers.list.ProvidersListScreen
 
 @ExperimentalMaterialApi
 @Composable
@@ -45,5 +49,23 @@ fun AppNavHost(navController: NavHostController) {
                 )
             }
         }
+
+        composable(ADD_ORDER_SCREEN) {
+            AddOrderScreen(
+                navigateBack = { navController.popBackStack() },
+                navigateToProvidersList = { navController.navigate(PROVIDERS_LIST_PICK_SCREEN) }
+            )
+        }
+
+        composable(PROVIDERS_LIST_PICK_SCREEN) {
+            ProvidersListScreen(
+                onProviderSelected = { provider ->
+                    val orderViewModel: OrdersViewModel = hiltViewModel()
+                    orderViewModel.onProviderSelected(provider)
+                    navController.popBackStack() // Go back to AddOrderScreen
+                }
+            )
+        }
+
     }
 }
