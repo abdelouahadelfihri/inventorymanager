129961e4a2d8e6b7ed0921904009a08918fd5ef0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-03 19:32:52 +0100

    updates to AddDeliveryContent.kt

--- app/src/main/java/com/example/inventorymanager/presentation/outgoings/deliveries/add/components/AddDeliveryContent.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/outgoings/deliveries/add/components/AddDeliveryContent.kt
@@ -1,4 +1,4 @@
-package com.example.inventorymanager.presentation.deliveries.add.components
+package com.example.inventorymanager.presentation.outgoings.deliveries.add.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
