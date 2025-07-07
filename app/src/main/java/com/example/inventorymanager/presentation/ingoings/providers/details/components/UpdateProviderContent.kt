273032633a752fd165395e1f898a4413cced3e19
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-07 20:03:27 +0100

    updates to UpdateProviderContent.kt

--- app/src/main/java/com/example/inventorymanager/presentation/ingoings/providers/details/components/UpdateProviderContent.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/ingoings/providers/details/components/UpdateProviderContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import com.example.inventorymanager.domain.model.outgoings.Customer
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import com.example.inventorymanager.domain.model.ingoings.Provider
