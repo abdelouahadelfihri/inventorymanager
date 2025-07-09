6d97058a95cf458d501e31d7d3707e616756a4a3
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-09 19:22:53 +0100

    updates to AddLocationTopBar.kt

--- app/src/main/java/com/example/inventorymanager/presentation/masterdata/locations/add/components/AddLocationTopBar.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/locations/add/components/AddLocationTopBar.kt
@@ -1,4 +1,4 @@
-package com.example.inventorymanager.presentation.locations.add.components
+package com.example.inventorymanager.presentation.masterdata.locations.add.components
 
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
