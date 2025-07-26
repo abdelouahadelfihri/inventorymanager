8b6ef1bb5039aca7586dfec7380650c5b19309a0
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-26 19:53:00 +0100

    updates to AddTransferTopBar.kt

--- app/src/main/java/com/example/inventorymanager/presentation/masterdata/transfers/add/components/AddTransferTopBar.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/transfers/add/components/AddTransferTopBar.kt
@@ -1,32 +1,17 @@
-package com.example.inventorymanager.presentation.masterdata.transfers.add.components
+package com.example.inventorymanager.presentation.transfers.add.components
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
-import com.example.inventorymanager.core.Constants.Companion.ADD_ORDER
 
 @Composable
-fun AddOrderTopBar(
-    navigateBack: () -> Unit
-) {
-    TopAppBar (
-        title = {
-            Text(
-                text = ADD_ORDER
-            )
-        },
+fun AddTransferTopBar(navigateBack: () -> Unit) {
+    TopAppBar(
+        title = { Text("Add Transfer") },
         navigationIcon = {
-            IconButton(
-                onClick = navigateBack
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.ArrowBack,
-                    contentDescription = null,
-                )
+            IconButton(onClick = navigateBack) {
+                Icon(Icons.Outlined.ArrowBack, contentDescription = "Back")
             }
         }
     )
