70b3123fb4b9d0f137a43d1f09f9bea7cd304ea2
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-27 19:49:55 +0100

    updates to products folder

---
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/products/details/UpdateLocationScreen.kt
@@ -0,0 +1,40 @@
+package com.example.inventorymanager.presentation.masterdata.products.details
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.inventorymanager.presentation.masterdata.locations.LocationsViewModel
+import com.example.inventorymanager.presentation.masterdata.locations.details.components.UpdateLocationContent
+import com.example.inventorymanager.presentation.masterdata.locations.details.components.UpdateLocationTopBar
+
+@Composable
+fun UpdateLocationScreen(
+    viewModel: LocationsViewModel = hiltViewModel(),
+    locationId: Int,
+    navigateBack: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        viewModel.getLocation(locationId)
+    }
+    Scaffold(
+        topBar = {
+            UpdateLocationTopBar(
+                navigateBack = navigateBack
+            )
+        },
+        content = { padding ->
+            UpdateLocationContent(
+                padding = padding,
+                location = viewModel.location,
+                updateLocation = { location ->
+                    viewModel.updateLocation(location)
+                },
+                deleteLocation = { locationId ->
+                    viewModel.deleteLocation(locationId)
+                },
+                navigateBack = navigateBack
+            )
+        }
+    )
+}
