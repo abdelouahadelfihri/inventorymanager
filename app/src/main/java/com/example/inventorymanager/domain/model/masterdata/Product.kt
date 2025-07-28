10134b25e8c7b2725f0de18cf3c4500c8338730d
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-28 19:52:18 +0100

    updates to Product.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/masterdata/Product.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/masterdata/Product.kt
@@ -28,23 +28,16 @@ import java.time.LocalDateTime
         Index(value = ["unit"])
     ]
 )
+
 data class Product(
     @PrimaryKey(autoGenerate = true)
     val productId: Int,
+    val name: String,
     val code: String,
     val barCode: String,
-    val name: String,
-    val sku: String,
     val category: Int,
     val unit: Int,
-    val description: String,
-    val reorderQuantity: Int,
     val reorderLevel: Int,
-    val packedWeight: Double,
-    val packedHeight: Double,
-    val packedWidth: Double,
-    val packedDepth: Double,
-    val refrigerated: Boolean,
     val isActive: Boolean,
     val createdAt: LocalDateTime,
 )
