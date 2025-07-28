3ee2f113073e84cbc4ad410e58b2a04821b8f5ec
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-28 19:45:35 +0100

    updates to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.inventorymanager"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.inventorymanager"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
