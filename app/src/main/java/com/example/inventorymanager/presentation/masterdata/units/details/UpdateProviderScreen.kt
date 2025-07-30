b887efc85433f9e1d02ab6b6a4bfa785ed5f761c
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-30 19:17:38 +0100

    updates to UpdateProviderScreen.kt

---
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/units/details/UpdateProviderScreen.kt
@@ -0,0 +1,41 @@
+package com.example.inventorymanager.presentation.masterdata.units.details
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.inventorymanager.presentation.ingoings.providers.ProvidersViewModel
+import com.example.inventorymanager.presentation.ingoings.providers.details.components.UpdateProviderContent
+import com.example.inventorymanager.presentation.ingoings.providers.details.components.UpdateProviderTopBar
+
+
+@Composable
+fun UpdateProviderScreen(
+    viewModel: ProvidersViewModel = hiltViewModel(),
+    providerId: Int,
+    navigateBack: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        viewModel.getProvider(providerId)
+    }
+    Scaffold(
+        topBar = {
+            UpdateProviderTopBar(
+                navigateBack = navigateBack
+            )
+        },
+        content = { padding ->
+            UpdateProviderContent(
+                padding = padding,
+                provider = viewModel.provider,
+                updateProvider = { provider ->
+                    viewModel.updateProvider(provider)
+                },
+                deleteProvider = { providerId ->
+                    viewModel.deleteProvider(providerId)
+                },
+                navigateBack = navigateBack
+            )
+        }
+    )
+}
