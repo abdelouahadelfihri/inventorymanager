ffdf022a5ac5cb3cb8aff65ef84597c6c2bc492a
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-07-31 20:29:52 +0100

    updates to AddUnitScreen.kt

--- app/src/main/java/com/example/inventorymanager/presentation/masterdata/units/add/AddUnitScreen.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/units/add/AddUnitScreen.kt
@@ -3,28 +3,26 @@ package com.example.inventorymanager.presentation.masterdata.units.add
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.inventorymanager.presentation.providers.ProvidersViewModel
-import com.example.inventorymanager.presentation.ingoings.providers.add.components.AddProviderContent
-import com.example.inventorymanager.presentation.ingoings.providers.add.components.AddProviderTopBar
+import com.example.inventorymanager.presentation.masterdata.units.UnitsViewModel
 
 
 @Composable
 fun AddUnitScreen(
-    viewModel: ProvidersViewModel = hiltViewModel(),
+    viewModel: UnitsViewModel = hiltViewModel(),
     navigateBack: () -> Unit
 ) {
     Scaffold(
         topBar = {
-            AddProviderTopBar(
+            AddUnitTopBar(
                 navigateBack = navigateBack
             )
         },
         content = { padding ->
-            AddProviderContent(
+            AddUnitContent(
                 padding = padding,
-                provider = viewModel.provider.value,
-                addProvider = { provider ->
-                    viewModel.addProvider(provider)
+                unit = viewModel.unit,
+                addUnit = { unit ->
+                    viewModel.addUnit(unit)
                 },
                 navigateBack = navigateBack
             )
