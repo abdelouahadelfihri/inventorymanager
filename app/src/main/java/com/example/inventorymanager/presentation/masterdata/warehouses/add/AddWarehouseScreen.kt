d898f35f7c377bce8dff26ed505352db296bbf78
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-08-17 15:50:13 +0100

    updates to AddWarehouseScreen.kt

--- app/src/main/java/com/example/inventorymanager/presentation/masterdata/warehouses/add/AddWarehouseScreen.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/warehouses/add/AddWarehouseScreen.kt
@@ -1,16 +1,12 @@
-package com.example.inventorymanager.presentation.warehouses.add
+package com.example.inventorymanager.presentation.masterdata.warehouses.add
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.inventorymanager.presentation.providers.ProvidersViewModel
-import com.example.inventorymanager.presentation.providers.add.components.AddProviderContent
-import com.example.inventorymanager.presentation.providers.add.components.AddProviderTopBar
 
 
 @Composable
-fun AddProviderScreen(
+fun AddWarehouseScreen(
     viewModel: ProvidersViewModel = hiltViewModel(),
     navigateBack: () -> Unit
 ) {
