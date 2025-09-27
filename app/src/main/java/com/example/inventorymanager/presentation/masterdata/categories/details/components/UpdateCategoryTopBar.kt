e173c7128e040a46927d71401688b49f59322217
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-09-27 18:39:13 +0100

    updates to UpdateCategoryTopBar.kt

--- app/src/main/java/com/example/inventorymanager/presentation/masterdata/categories/details/components/UpdateCategoryTopBar.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/masterdata/categories/details/components/UpdateCategoryTopBar.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
-import com.example.inventorymanager.core.Constants.Companion.UPDATE_CATEGORY_SCREEN
+import com.example.inventorymanager.core.Constants.Companion.UPDATE_CATEGORY
 
 @Composable
 fun UpdateCategoryTopBar(
@@ -16,7 +16,7 @@ fun UpdateCategoryTopBar(
     TopAppBar (
         title = {
             Text(
-                text = UPDATE_CATEGORY_SCREEN
+                text = UPDATE_CATEGORY
             )
         },
         navigationIcon = {
