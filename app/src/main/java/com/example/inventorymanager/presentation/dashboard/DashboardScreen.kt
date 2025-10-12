2e2941e978d59bb1eaec6b6030db138efc3e6382
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-10-12 17:27:03 +0100

    updates to DashboardScreen.kt

--- app/src/main/java/com/example/inventorymanager/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/inventorymanager/presentation/dashboard/DashboardScreen.kt
@@ -102,12 +102,15 @@ fun DrawerMenu(onSelectItem: (String) -> Unit) {
         DrawerItem("Providers", Icons.Default.ArrowDownward) { onSelectItem("Providers") }
         DrawerItem("Orders", Icons.Default.ArrowDownward) { onSelectItem("Orders") }
         DrawerItem("Purchases Receipts", Icons.Default.ArrowDownward) { onSelectItem("Purchases Receipts") }
+        DrawerItem("Returns", Icons.Default.ArrowDownward) { onSelectItem("Returns") }
 
         Divider(modifier = Modifier.padding(vertical = 8.dp))
 
         // Outgoings
         DrawerItem("Customers", Icons.Default.ArrowUpward) { onSelectItem("Customers") }
         DrawerItem("Deliveries", Icons.Default.ArrowUpward) { onSelectItem("Deliveries") }
+        DrawerItem("Sales Receipts", Icons.Default.ArrowUpward) { onSelectItem("Sales Receipts") }
+        DrawerItem("Returns", Icons.Default.ArrowUpward) { onSelectItem("Returns") }
 
         Divider(modifier = Modifier.padding(vertical = 8.dp))
 
