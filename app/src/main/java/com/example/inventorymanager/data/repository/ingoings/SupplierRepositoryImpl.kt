3f717977bdeecc0bb5dc15531d1e51c32db1ce80
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-10-18 21:15:34 +0100

    updates to SupplierRepositoryImpl.kt

---
+++ app/src/main/java/com/example/inventorymanager/data/repository/ingoings/SupplierRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.example.inventorymanager.data.repository.ingoings
+
+import com.example.inventorymanager.data.dao.ingoings.SupplierDao
+import com.example.inventorymanager.domain.model.ingoings.Supplier
+import com.example.inventorymanager.domain.repository.ingoings.ProviderRepository
+
+class SupplierRepositoryImpl(
+    private val supplierDao: SupplierDao
+) : SupplierRepository {
+
+    override fun getSuppliersFromRoom() = supplierDao.getSuppliers()
+
+    override suspend fun getSupplierFromRoom(id: Int) = supplierDao.getSupplier(id)
+
+    override suspend fun addSupplierToRoom(supplier: Supplier) = supplierDao.addSupplier(supplier)
+
+    override suspend fun updateSupplierInRoom(supplier: Supplier) = supplierDao.updateSupplier(supplier)
+
+    override suspend fun deleteSupplierFromRoom(id: Int) = supplierDao.deleteSupplier(id)
+
+}
