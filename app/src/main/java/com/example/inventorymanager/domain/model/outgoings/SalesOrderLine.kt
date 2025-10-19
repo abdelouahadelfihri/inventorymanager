3ac3adc562b68665e2a1b4495d73d33eef5a67df
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-10-19 12:17:46 +0100

    updates to SalesOrderLine.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/outgoings/SalesOrderLine.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/outgoings/SalesOrderLine.kt
@@ -5,19 +5,41 @@ import androidx.room.Entity
 import androidx.room.Index
 import com.example.inventorymanager.domain.model.masterdata.Product
 import com.example.inventorymanager.core.Constants.Companion.SALES_ORDER_LINE_TABLE
+import com.example.inventorymanager.domain.model.masterdata.Warehouse
 
 @Entity(
     tableName = SALES_ORDER_LINE_TABLE,
     primaryKeys = ["salesOrderId", "productId", "warehouseId"],
     foreignKeys = [
-        ForeignKey(entity = SalesOrder::class, parentColumns = ["deliveryId"], childColumns = ["deliveryId"], onDelete = ForeignKey.CASCADE),
-        ForeignKey(entity = Product::class, parentColumns = ["productId"], childColumns = ["productId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(
+            entity = SalesOrder::class,
+            parentColumns = ["id"],
+            childColumns = ["salesOrderId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Product::class,
+            parentColumns = ["id"],
+            childColumns = ["productId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Warehouse::class,
+            parentColumns = ["id"],
+            childColumns = ["warehouseId"],
+            onDelete = ForeignKey.CASCADE
+        )
     ],
-    indices = [Index(value = ["deliveryId"]), Index(value = ["productId"]), Index(value = ["warehouseId"])]
+    indices = [
+        Index(value = ["salesOrderId"]),
+        Index(value = ["productId"]),
+        Index(value = ["warehouseId"])
+    ]
 )
 data class SalesOrderLine(
     val salesOrderId: Long,
     val productId: Long,
+    val warehouseId: Long,
     val quantity: Double,
-    val unitPrice: Double,
+    val unitPrice: Double
 )
