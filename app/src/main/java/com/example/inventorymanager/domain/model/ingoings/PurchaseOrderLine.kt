1966f0e757f1a7d1e4b19aee0ba3f18ad68f6e85
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-10-19 12:42:10 +0100

    updates to PurchaseOrderLine.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseOrderLine.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/ingoings/PurchaseOrderLine.kt
@@ -1,28 +1,45 @@
-package com.example.inventorymanager.domain.model.ingoings
+package com.example.inventorymanager.domain.model.outgoings
 
 import androidx.room.ForeignKey
 import androidx.room.Entity
 import androidx.room.Index
-import androidx.room.PrimaryKey
-import com.example.inventorymanager.core.Constants.Companion.ORDER_DETAIL_TABLE
-import com.example.inventorymanager.domain.model.masterdata.Warehouse
 import com.example.inventorymanager.domain.model.masterdata.Product
-
+import com.example.inventorymanager.core.Constants.Companion.SALES_ORDER_LINE_TABLE
+import com.example.inventorymanager.domain.model.masterdata.Warehouse
 
 @Entity(
-    tableName = ORDER_DETAIL_TABLE,
-    primaryKeys = ["orderId", "productId","warehouseId"],
+    tableName = SALES_ORDER_LINE_TABLE,
+    primaryKeys = ["purchaseOrderId", "productId", "warehouseId"],
     foreignKeys = [
-        ForeignKey(entity = PurchaseOrder::class, parentColumns = ["orderId"], childColumns = ["orderId"], onDelete = ForeignKey.CASCADE),
-        ForeignKey(entity = Product::class, parentColumns = ["productId"], childColumns = ["productId"], onDelete = ForeignKey.CASCADE),
-        ForeignKey(entity = Warehouse::class, parentColumns = ["warehouseId"], childColumns = ["warehouseId"], onDelete = ForeignKey.CASCADE)
+        ForeignKey(
+            entity = SalesOrder::class,
+            parentColumns = ["id"],
+            childColumns = ["purchaseOrderId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Product::class,
+            parentColumns = ["id"],
+            childColumns = ["productId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Warehouse::class,
+            parentColumns = ["id"],
+            childColumns = ["warehouseId"],
+            onDelete = ForeignKey.CASCADE
+        )
     ],
-    indices = [Index(value = ["orderId"]), Index(value = ["productId"]), Index(value = ["warehouseId"])]
+    indices = [
+        Index(value = ["salesOrderId"]),
+        Index(value = ["productId"]),
+        Index(value = ["warehouseId"])
+    ]
 )
 data class PurchaseOrderLine(
-    @PrimaryKey(autoGenerate = true)
-    val orderId: Int,
-    val productId: Int,
-    val warehouseId: Int,
-    val quantity: Int
+    val purchaseOrderId: Long,
+    val productId: Long,
+    val warehouseId: Long,
+    val quantity: Double,
+    val unitPrice: Double
 )
