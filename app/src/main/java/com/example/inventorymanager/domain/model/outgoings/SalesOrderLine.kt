e5c110b827ee910d0e947250b02769f2cdace651
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-10-19 12:07:57 +0100

    updates to SalesOrderLine.kt

--- app/src/main/java/com/example/inventorymanager/domain/model/outgoings/SalesOrderLine.kt
+++ app/src/main/java/com/example/inventorymanager/domain/model/outgoings/SalesOrderLine.kt
@@ -3,23 +3,23 @@ package com.example.inventorymanager.domain.model.outgoings
 import androidx.room.ForeignKey
 import androidx.room.Entity
 import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.example.inventorymanager.domain.model.masterdata.Item
 import com.example.inventorymanager.core.Constants.Companion.DELIVERY_DETAILS_TABLE
 import com.example.inventorymanager.domain.model.masterdata.Warehouse
 
 @Entity(
     tableName = DELIVERY_DETAILS_TABLE,
-    primaryKeys = ["deliveryId", "productId", "warehouseId"],
+    primaryKeys = ["salesOrderId", "productId", "warehouseId"],
     foreignKeys = [
         ForeignKey(entity = SalesOrder::class, parentColumns = ["deliveryId"], childColumns = ["deliveryId"], onDelete = ForeignKey.CASCADE),
         ForeignKey(entity = Item::class, parentColumns = ["productId"], childColumns = ["productId"], onDelete = ForeignKey.CASCADE),
-        ForeignKey(entity = Warehouse::class, parentColumns = ["warehouseId"], childColumns = ["warehouseId"], onDelete = ForeignKey.CASCADE)
     ],
     indices = [Index(value = ["deliveryId"]), Index(value = ["productId"]), Index(value = ["warehouseId"])]
 )
 data class SalesOrderLine(
-    val deliveryId: Int,
-    val productId: Int,
-    val warehouseId: Int,
-    val quantity: Int
+    val salesOrderId: Long,
+    val productId: Long,
+    val quantity: Double,
+    val unitPrice: Double,
 )
